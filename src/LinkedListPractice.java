edb57ae66d214c6689f8b96b7e052283b2aa3815
Aryan-Sajith <aryan.sajith04@gmail.com>
2022-06-21 23:06:41 -0400

    My implementation of a LinkedList Class

--- src/LinkedListPractice.java
+++ src/LinkedListPractice.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class LinkedListPractice {
+public class LinkedListPractice {
+    public static void main(String[] args) {
+        MyLinkedList fun = new MyLinkedList();
+        fun.addAtIndex(0,10);
+        fun.addAtIndex(0,20);
+
+        for(int i = 0; i < fun.getSize(); i++) {
+            System.out.println(fun.get(i));
+        }
+    }
 }
