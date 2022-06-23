5822d5dcaf466007ffd458303a1af6eafb053af4
Aryan-Sajith <aryan.sajith04@gmail.com>
2022-06-23 12:39:27 -0400

    SinglyLinkedList Code w/ Sentinel Node

---
+++ src/LinkedList/SinglyLinkedList.java
@@ -0,0 +1,78 @@
+package LinkedList;
+
+class SinglyLinkedList {
+    public class Node {
+        int val;
+        Node next;
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    private int size;
+    private Node head;//sentinel head node
+
+    public SinglyLinkedList() {
+        size = 0;
+        head = new Node(0);
+    }
+
+    public int get(int index) {
+        //first we check for invalid indices
+        if(index < 0 || index >= size) {
+            return -1;
+        }
+        //here we get the i-th node
+        else {
+            Node curr = head;
+            for(int i = 0; i < index+1;i++) {
+                curr = curr.next;
+            }
+            return curr.val;
+        }
+    }
+
+    public void addAtHead(int val) {
+        addAtIndex(0,val);
+    }
+
+    public void addAtTail(int val) {
+        addAtIndex(size,val);
+    }
+
+    public void addAtIndex(int index, int val) {
+        //an invalid index
+        if(index>size) {
+            return;
+        }
+        //valid index here
+        else {
+            Node curr = head;
+            for(int i = 0; i < index; i++) {
+                curr = curr.next;
+            }
+            Node toAdd = new Node(val); //the new node to be added
+            toAdd.next = curr.next;
+            curr.next = toAdd;
+            size++; //we're increasing size by adding a node
+        }
+    }
+
+    public void deleteAtIndex(int index) {
+        if(index < 0 || index >= size) {
+            return;
+        }
+        Node curr = head;
+        for(int i = 0; i < index; i++) {
+            curr = curr.next;
+        }
+        curr.next = curr.next.next;
+        size--; //because we are deleting a node
+    }
+
+    //getter method for size
+    public int getSize() {
+        return size;
+    }
+
+}
---
+++ src/LinkedList/SinglyLinkedListPractice.java
@@ -0,0 +1,16 @@
+package LinkedList;
+
+public class SinglyLinkedListPractice {
+    public static void main(String[] args) {
+        SinglyLinkedList fun = new SinglyLinkedList();
+        fun.addAtIndex(0,1);
+        fun.addAtTail(6);
+        fun.addAtHead(15);
+        fun.addAtIndex(2,21);
+        fun.deleteAtIndex(1);
+
+        for(int i = 0; i < fun.getSize(); i++) {
+            System.out.println(fun.get(i));
+        }
+    }
+}
