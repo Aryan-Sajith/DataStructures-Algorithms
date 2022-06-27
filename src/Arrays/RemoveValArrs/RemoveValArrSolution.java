51cb4972d7dca51b229fc357087f93f90ce0aad2
Aryan-Sajith <aryan.sajith04@gmail.com>
2022-06-27 18:33:13 -0400

    RemoveValFromArray Problem and Solution with a Test Class

---
+++ src/Arrays/RemoveValArrs/RemoveValArrSolution.java
@@ -0,0 +1,22 @@
+package Arrays.RemoveValArrs;
+
+public class RemoveValArrSolution {
+    public static int removeElement(int[] nums, int val) {
+        int i = 0, length = nums.length;
+
+        //while loop bc we won't move on from curr index
+        //until we're sure it's non-val
+        while(i<length) {
+            if(nums[i] == val){
+                nums[i] = nums[length-1];
+                length--;
+            }
+            else {
+                i++;
+            }
+        }
+        return i;
+    }
+    //Time Complexity: O(N) if  we remove all values
+    //Space Complexity: O(1) bc no extra memory
+}
---
+++ src/Arrays/RemoveValArrs/RemoveValArrTest.java
@@ -0,0 +1,24 @@
+package Arrays.RemoveValArrs;
+
+
+public class RemoveValArrTest {
+    public static void main(String[] args) {
+        int[] nums = {1,4,6,2,0,13,6,12,42,21,23,6,6,2}; // original array
+        int[] expectedNums = {1,4,2,0,13,12,42,21,23,2}; //array with val removed
+
+        int output = RemoveValArrSolution.removeElement(nums, 6); //returns length of array after Val is deleted
+
+
+        assert output == expectedNums.length;
+        int nowSize = 0;
+        for (int i = 0; i < output; i++) {
+            nowSize++;
+        }
+        if(nowSize == expectedNums.length) {
+            System.out.println("Your program has worked! The Val has been deleted");
+        }
+        else {
+            System.out.println("Your program has failed! The Val has not been deleted");
+        }
+    }
+}
