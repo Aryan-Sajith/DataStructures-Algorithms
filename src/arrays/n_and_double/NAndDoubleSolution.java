a90e561b74878252fdd3ebd67ed818ec707fae48
Aryan-Sajith <aryan.sajith04@gmail.com>
2022-06-28 14:12:10 -0400

    N and it's double Problem with Quadratic Time and Constant Time. Another approach with Hashmaps utilizes linear time and space complexity(come back for that later!)

---
+++ src/arrays/n_and_double/NAndDoubleSolution.java
@@ -0,0 +1,33 @@
+package arrays.n_and_double;
+
+public class NAndDoubleSolution {
+    public static boolean checkIfExist(int[] arr) {
+        int cap = arr.length;
+
+        // Handles array edge cases
+        if(arr == null || cap == 0) {
+            return false;
+        }
+
+        // Checks if a multiple of 2 and it's halfValue are present in the array
+        for(int i = 0; i < cap; i++) {
+            if(arr[i] % 2 == 0) {
+                for(int halfValFinder = 0; halfValFinder < cap; halfValFinder++) {
+                    if(halfValFinder == i) {
+                        continue;
+                    }
+                    if(arr[halfValFinder] * 2 == arr[i]) {
+                        return true;
+                    }
+                }
+            }
+
+        }
+
+        return false;
+    }
+
+    /*Time Complexity: O(N^2). No sorting and only using arrays.
+      Space Complexity: O(1)
+    */
+}
---
+++ src/arrays/n_and_double/NAndDoubleSolutionTest.java
@@ -0,0 +1,10 @@
+package arrays.n_and_double;
+
+public class NAndDoubleSolutionTest {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,5,6};
+
+        System.out.println("The array nums contains a number n and its double--> " +
+                NAndDoubleSolution.checkIfExist(nums));
+    }
+}
